4895316e7a0a6e995a0cb0633d018d3217e38fe4
crash2bash <sachagurov@gmail.com>
2020-03-06 20:52:29 +0200

    add script

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,35 @@
 const shippingPage = document.querySelector(".cart");
 const showShippingPage = document.querySelector(".cart--buy");
 const buyButton = document.querySelector(".button--question");
+const deleteProductButton = document.querySelector(".product-card__close--gold");
+const goldSpeakerCard = document.querySelector(".product-card--gold");
+const addProductItem = document.querySelector(".cart__price-up");
+const valueOfProduct = document.querySelectorAll(".cart__value");
+const removeProductItem = document.querySelector(".cart__price-down");
+let inputValue = 1;
 
-buyButton.addEventListener("click", function(e) {
+buyButton.addEventListener("click", function() {
   shippingPage.classList.add("cart--buy");
-})
+});
+
+deleteProductButton.addEventListener("click", function() {
+  goldSpeakerCard.classList.add("hide");
+});
+
+addProductItem.addEventListener("click", function() {
+  inputValue += 1;
+  for (let i in valueOfProduct) {
+    if ((valueOfProduct[i].value < 10) && (valueOfProduct[i].value > 0)) {
+      valueOfProduct[i].value = inputValue;
+    }
+  }
+});
+
+removeProductItem.addEventListener("click", function() {
+  inputValue -= 1;
+  for (let i in valueOfProduct) {
+    if (valueOfProduct[i].value > 1) {
+      valueOfProduct[i].value = inputValue;
+    }
+  }
+});
